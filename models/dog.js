9a487eca553ad1a4ffb101621c7d084d3ed86fe9
sowmya-yalamarthi <s542049@nwmissouri.edu>
2021-04-23 14:05:31 -0500

    changes made

--- models/dog.js
+++ models/dog.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose")
 const dogSchema = mongoose.Schema({
-dogname: String,
-age: Number,
-breed: String
+dogname: {
+    type: String,
+    minlength: 4,
+    maxlength: 14,
+},
+age: {
+    type: Number,
+    max: [100, "Age should be less than 100"],
+    min: [0, "Age should be more than 0"]
+},
+breed: {
+    type: String,
+    required: [true, "breed is requried"]
+}
 })
 module.exports = mongoose.model("dog", dogSchema)
--- routes/dog.js
+++ routes/dog.js
@@ -1,15 +1,19 @@
 var express = require('express');
 const dog_controlers= require('../controllers/dog');
 var router = express.Router();
-/* GET dogs */
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+/* GET dog */
 router.get('/', dog_controlers.dog_view_all_Page );
-/* GET detail dog page */
-router.get('/detail', dog_controlers.dog_view_one_Page);
-/* GET create dog page */
-router.get('/create', dog_controlers.dog_create_Page);
-/* GET create update page */
-router.get('/update', dog_controlers.dog_update_Page);
-/* GET create dog page */
-router.get('/delete', dog_controlers.dog_delete_Page);
-
+/* GET update dog page */
+router.get('/update', secured, dog_controlers.dog_update_Page);
+router.get('/create', secured, dog_controlers.dog_create_Page);
+router.get('/delete', secured, dog_controlers.dog_delete_Page);
 module.exports = router;
